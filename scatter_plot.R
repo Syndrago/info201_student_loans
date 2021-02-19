e9312dc3970ee599854067f961922013df9b8005
AlaskanLuke <ljms@uw.edu>
2021-02-18 20:33:26 -0800

    Made scatterplot

---
+++ scatter_plot.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+dash <- read.csv(file = "data/dashboard_data.csv")
+
+
+ggplot(data = dash) +
+  geom_point(mapping = aes(x = Subsidized.Recipients, y = Subsidized.Dollars.of.Disbursements),
+  alpha = 0.3,
+  size = 3,
+  color = "dodgerblue2"
+  ) +
+  labs(y = "Dollar Amount", x = "Number of Subsidized Recipients", title = "Total Number of Recipients VS Amount Given") +
+  scale_y_continuous(labels = scales::comma) +
+  theme(plot.title = element_text(hjust = 0.5))
