42fe3a2aaff7a302d61f54c373dbdd625fee4184
legendui <77509046+legendui@users.noreply.github.com>
2021-02-19 15:17:11 -0800

    A Simple Barchart

---
+++ barchart.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(dplyr)
+database <- read.csv("https://raw.githubusercontent.com/info201a-w21/info201_student_loans/main/data/dashboard_data.csv?token=ASPLDNWOLRU7NNTQEHOFDFDAHBZRE")
+national_ave <- mean(database$Subsidized.Dollars.of.Disbursements, na.rm = T) %>%
+  round()
+
+wa_ave <- database %>%
+  filter(State == "WA") %>%
+  summarise(state_average = mean(Subsidized.Dollars.of.Disbursements, na.rm = T)) %>%
+  pull(state_average) %>%
+  round()
+
+df <- data.frame(
+  name = c("National Average", "WA Average"),
+  loan = c(national_ave, wa_ave)
+)
+
+p <- ggplot(data = df, aes(x = name, y = loan)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = loan),
+    vjust = 1.6,
+    color = "white", size = 3.5
+  ) +
+  theme_minimal() +
+  labs(
+    y = "Dollar Amount", x = "",
+    title = "National Vs WA Student Loan Subsidized Disburements"
+  )
+
