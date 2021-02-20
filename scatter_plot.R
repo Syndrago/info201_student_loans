cef81ac1f4724a11253777de4de81645dd66189b
AlaskanLuke <ljms@uw.edu>
2021-02-19 16:59:00 -0800

    Styling

--- scatter_plot.R
+++ scatter_plot.R
@@ -4,11 +4,14 @@ dash <- read.csv(file = "data/dashboard_data.csv")
 
 
 scatter_plot <- ggplot(data = dash) +
-  geom_point(mapping = aes(x = Subsidized.Recipients, y = Subsidized.Dollars.of.Disbursements),
-  alpha = 0.3,
-  size = 3,
-  color = "dodgerblue2"
+  geom_point(
+    mapping = aes(x = Subsidized.Recipients,
+                  y = Subsidized.Dollars.of.Disbursements),
+    alpha = 0.3,
+    size = 3,
+    color = "dodgerblue2"
   ) +
-  labs(y = "Dollar Amount", x = "Number of Subsidized Recipients", title = "Total Number of Recipients VS Amount Given") +
+  labs(y = "Dollar Amount", x = "Number of Subsidized Recipients",
+       title = "Total Number of Recipients VS Amount Given") +
   scale_y_continuous(labels = scales::comma) +
   theme(plot.title = element_text(hjust = 0.5))
--- summary.R
+++ summary.R
@@ -1,20 +1,21 @@
 library(tidyverse)
 data <- read.csv(file = "data/dashboard_data.csv")
-data$Subsidized.Recipients <- suppressWarnings(as.numeric(gsub(",", "", data$Subsidized.Recipients, fixed = TRUE)))
+data$Subsidized.Recipients <- suppressWarnings(as.numeric(gsub(",",
+  "", data$Subsidized.Recipients, fixed = TRUE)))
 
 summary_info <- list()
 
-summary_info$highest_recipient_name <- data %>% 
-  filter(Subsidized.Recipients == max(Subsidized.Recipients, na.rm = T)) %>% 
+summary_info$highest_recipient_name <- data %>%
+  filter(Subsidized.Recipients == max(Subsidized.Recipients, na.rm = T)) %>%
   pull(School)
 
-summary_info$average_recipients <- round(mean(data$Subsidized.Recipients, na.rm = T))
+summary_info$average_recipients <- round(mean(
+  data$Subsidized.Recipients, na.rm = T))
 
 summary_info$num_colleges <- nrow(data)
 
-summary_info$average_amount <- round(mean(data$Subsidized.Dollars.of.Disbursements, na.rm = T))
+summary_info$average_amount <- round(mean(
+  data$Subsidized.Dollars.of.Disbursements, na.rm = T))
 
-summary_info$highest_amount <- round(max(data$Subsidized.Dollars.of.Disbursements, na.rm = T))
-
-
-
+summary_info$highest_amount <- round(max(
+  data$Subsidized.Dollars.of.Disbursements, na.rm = T))
