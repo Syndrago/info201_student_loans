a586058e25a744d8abd78f8554a96a36b9dd2c6b
legendui <77509046+legendui@users.noreply.github.com>
2021-03-16 20:41:34 -0700

    chart3

--- final_project/server.R
+++ final_project/server.R
@@ -7,99 +7,123 @@ library(ggplot2)
 library(DT)
 
 
-loan_data <- read.csv(file = 'dashboard_data.csv')
+loan_data <- read.csv(file = "dashboard_data.csv")
 
+# create a table for average loan of each school
+data_for_char3 <- loan_data %>%
+  mutate(average_sub = Subsidized.Dollars.of.Loans.Originated
+  / Subsidized.Num.of.Loans.Originated) %>%
+  filter(School.Type == "Public") %>%
+  select(School, State, average_sub)
 
 # Heatmap -----------------------------------------------------------------
-popdata <- read.csv(file = '2020.csv')
+popdata <- read.csv(file = "2020.csv")
 
-filt <- popdata %>% 
-    select(NAME, POPESTIMATE2020)
+filt <- popdata %>%
+  select(NAME, POPESTIMATE2020)
 
-filt <- filt[6:56,]
+filt <- filt[6:56, ]
 
 filt$NAME <- state.abb[match(tolower(filt$NAME), tolower(state.name))]
 
-filt <- filt %>% 
-    rename(State = NAME, Population = POPESTIMATE2020)
+filt <- filt %>%
+  rename(State = NAME, Population = POPESTIMATE2020)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  output$map <- renderPlot({
+    selected <- loan_data %>%
+      select(
+        3:7,
+        Unsubsidized.Undergrad.Recipients,
+        Unsubsidized.Graduate.Recipients,
+        Parent.Plus.Recipients
+      )
 
-    output$map <- renderPlot({
-        
-        selected <- loan_data %>% 
-            select(3:7,
-                   Unsubsidized.Undergrad.Recipients,
-                   Unsubsidized.Graduate.Recipients,
-                   Parent.Plus.Recipients) 
-            
-        assign("drop", input$var) # get(input)
-        
-        filt_data <- selected %>% 
-            group_by(State) %>%
-            summarise(state_total = sum(get(drop), na.rm = T)) %>% 
-            left_join(filt, by = "State") %>% 
-            na.omit() %>% 
-            mutate(percent_of_pop = round(state_total / Population, 4) * 100)
-        
-        state_shapes <- map_data("state") %>% # Get shape map of states
-            rename(State = region)
-        
-        state_shapes$State <- state.abb[match(state_shapes$State, tolower(state.name))]
-        
-        joined <- state_shapes %>% # Join state shapes and stats
-            left_join(filt_data, by = "State")
-        
-        if (input$bchoice == "Total") {
-            choice <- "state_total"
-            lab <- "Total Count"
-        } else {
-            choice <- "percent_of_pop"
-            lab <- "Rate"
-        }
-        
-        heatmap <- ggplot(joined) + # Create chart
-            geom_polygon(
-                mapping = aes(x = long, y = lat, group = group, fill = get(choice)),
-                color = "white",
-                size = .1
-            ) +
-            coord_map() +
-            scale_fill_continuous(low = "Green4", high = "Red", labels = comma) +
-            labs(fill = lab) +
-            theme(
-                axis.line = element_blank(),
-                axis.text = element_blank(),
-                axis.ticks = element_blank(),
-                axis.title = element_blank(),
-                plot.background = element_blank(),
-                panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-                panel.border = element_blank(),
-                panel.background = element_blank()
-            ) +
-            theme(legend.key.size = unit(1.5, 'cm'), legend.text = element_text(size=15),
-                  legend.title = element_text(size=25)) +
-            labs(title = paste("Heatmap of", input$var, "as", input$bchoice)) +
-            theme(plot.title = element_text(hjust = 0.5, size = 25, family="TT Times New Roman"))
-        
-        return(heatmap)
+    assign("drop", input$var) #get(input)
 
-    })
-    output$tabletitle <- renderText({
-        input$category
-    })
-    output$dollartable <- renderDataTable({
-        assign("dtype", input$category)
-        loantabled <-loan_data %>% 
-            filter(!is.na(get(dtype))) %>%
-            select(School, State, dtype) %>% 
-            filter(between(get(dtype),
-                           input$dollars[1],input$dollars[2])) %>%
-            arrange(School)
-        colnames(loantabled) <- c("School", "State", gsub("[.]", " ", dtype))
-        loantabled
-    })
+    filt_data <- selected %>%
+      group_by(State) %>%
+      summarise(state_total = sum(get(drop), na.rm = T)) %>%
+      left_join(filt, by = "State") %>%
+      na.omit() %>%
+      mutate(percent_of_pop = round(state_total / Population, 4) * 100)
 
+    state_shapes <- map_data("state") %>% # Get shape map of states
+      rename(State = region)
+
+    state_shapes$State <- state.abb[match(state_shapes$State, tolower(state.name))]
+
+    joined <- state_shapes %>% # Join state shapes and stats
+      left_join(filt_data, by = "State")
+
+    if (input$bchoice == "Total") {
+      choice <- "state_total"
+      lab <- "Total Count"
+    } else {
+      choice <- "percent_of_pop"
+      lab <- "Rate"
+    }
+
+    heatmap <- ggplot(joined) + # Create chart
+      geom_polygon(
+        mapping = aes(x = long, y = lat, group = group, fill = get(choice)),
+        color = "white",
+        size = .1
+      ) +
+      coord_map() +
+      scale_fill_continuous(low = "Green4", high = "Red", labels = comma) +
+      labs(fill = lab) +
+      theme(
+        axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()
+      ) +
+      theme(
+        legend.key.size = unit(1.5, "cm"), legend.text = element_text(size = 15),
+        legend.title = element_text(size = 25)
+      ) +
+      labs(title = paste("Heatmap of", input$var, "as", input$bchoice)) +
+      theme(plot.title = element_text(hjust = 0.5, size = 25, family = "TT Times New Roman"))
+
+    return(heatmap)
+  })
+  output$tabletitle <- renderText({
+    input$category
+  })
+  output$dollartable <- renderDataTable({
+    assign("dtype", input$category)
+    loantabled <- loan_data %>%
+      filter(!is.na(get(dtype))) %>%
+      select(School, State, dtype) %>%
+      filter(between(
+        get(dtype),
+        input$dollars[1], input$dollars[2]
+      )) %>%
+      arrange(School)
+    colnames(loantabled) <- c("School", "State", gsub("[.]", " ", dtype))
+    loantabled
+  })
+
+  output$title_3 <- renderText({
+    title_str <- paste0("Top ", input$x_val, " affordable schools in ", input$state)
+    title_str
+  })
+
+  output$top_school <- renderPlotly({
+    df <- data_for_char3 %>%
+      filter(State == input$state) %>%
+      arrange() %>%
+      top_n(input$x_val * -1)
+    bar_chart <- ggplot(df, aes(y = reorder(School, average_sub * -1), x = average_sub)) +
+      geom_bar(stat = "identity") +
+      labs(x = "Average Loan Per Recipient", y = "School")
+    bar_chart
+  })
 })
--- final_project/ui.R
+++ final_project/ui.R
@@ -7,76 +7,98 @@ source("server.R")
 # Chart Page Layout -------------------------------------------------------
 
 chart_layout <- sidebarLayout(
-        sidebarPanel(
-            selectInput(
-                inputId = "var",
-                label = "Variable of Interest",
-                choices = list(
-                    "Subsidized Recipients" = "Subsidized.Recipients",
-                    "Unsubsidized Graduate Recipients" = "Unsubsidized.Graduate.Recipients",
-                    "Unsubsidized Undergrad Recipients" = "Unsubsidized.Undergrad.Recipients",
-                    "Parent Recipients" = "Parent.Plus.Recipients"
-                )
-            ),
-            radioButtons(
-                inputId = "bchoice",
-                label = "Choice",
-                choices = c("Total", "Percent of Population")
-            ),
-            tags$div(
-                h4("Eplanation:"),
-                tags$p("Subsidized Recipients are those students who have
-                       recieved loans with interest payed for by the government."),
-                tags$p("Unsubsidized Graduate Recipients are those graduate students who recieved
-                       loans WITHOUT interest payed for bythe government."),
-                tags$p("Unsubsidized Undergrad Recipients are those undergraduate students
-                       who have recieved loans WITHOUT interest payed for by the government."),
-                tags$p("Parent (plus) Recipients are those parents who have recieved student
-                       loans on behalf of thier children.")
-            )
-            
+  sidebarPanel(
+    selectInput(
+      inputId = "var",
+      label = "Variable of Interest",
+      choices = list(
+        "Subsidized Recipients" = "Subsidized.Recipients",
+        "Unsubsidized Graduate Recipients" = "Unsubsidized.Graduate.Recipients",
+        "Unsubsidized Undergrad Recipients" = "Unsubsidized.Undergrad.Recipients",
+        "Parent Recipients" = "Parent.Plus.Recipients"
+      )
     ),
-    mainPanel(
-        plotOutput("map")
+    radioButtons(
+      inputId = "bchoice",
+      label = "Choice",
+      choices = c("Total", "Percent of Population")
+    ),
+    tags$div(
+      h4("Eplanation:"),
+      tags$p("Subsidized Recipients are those students who have
+                       recieved loans with interest payed for by the government."),
+      tags$p("Unsubsidized Graduate Recipients are those graduate students who recieved
+                       loans WITHOUT interest payed for bythe government."),
+      tags$p("Unsubsidized Undergrad Recipients are those undergraduate students
+                       who have recieved loans WITHOUT interest payed for by the government."),
+      tags$p("Parent (plus) Recipients are those parents who have recieved student
+                       loans on behalf of thier children.")
     )
+  ),
+  mainPanel(
+    plotOutput("map")
+  )
 )
 
 chart2_layout <- sidebarLayout(
-    sidebarPanel(
-        selectInput(
-            inputId = "category",
-            label = "Student Loan Type",
-            choices = list(
-                "Subsidized" = "Subsidized.Dollars.of.Loans.Originated",
-                "Unsubsidized Undergraduate" = "Unsubsidized.Undergrad.Dollars.of.Loans.Originated",
-                "Unsubsidized Graduate" = "Unsubsidized.Graduate.Dollars.of.Loans.Originated",
-                "Parent Plus" = "Parent.Plus.Dollars.of.Loans.Originated",
-                "Graduate Plus" = "Grad.Plus.Dollars.of.Loans.Originated")
-        ),
-        sliderInput(
-            inputId = "dollars",
-            label = "Choose dollar range",
-            min = 0,
-            max = max(loan_data$Unsubsidized.Graduate.Dollars.of.Loans.Originated, na.rm = T),
-            value = c(5200000,136000000)
-        )
+  sidebarPanel(
+    selectInput(
+      inputId = "category",
+      label = "Student Loan Type",
+      choices = list(
+        "Subsidized" = "Subsidized.Dollars.of.Loans.Originated",
+        "Unsubsidized Undergraduate" = "Unsubsidized.Undergrad.Dollars.of.Loans.Originated",
+        "Unsubsidized Graduate" = "Unsubsidized.Graduate.Dollars.of.Loans.Originated",
+        "Parent Plus" = "Parent.Plus.Dollars.of.Loans.Originated",
+        "Graduate Plus" = "Grad.Plus.Dollars.of.Loans.Originated"
+      )
     ),
-    mainPanel(
-        dataTableOutput("dollartable")
+    sliderInput(
+      inputId = "dollars",
+      label = "Choose dollar range",
+      min = 0,
+      max = max(loan_data$Unsubsidized.Graduate.Dollars.of.Loans.Originated, na.rm = T),
+      value = c(5200000, 136000000)
     )
+  ),
+  mainPanel(
+    dataTableOutput("dollartable")
+  )
 )
 
+chart3_layout <- sidebarLayout(
+  sidebarPanel(
+    selectInput(
+      inputId = "state",
+      label = "Choose a State",
+      choices = filt$State,
+      selected = "WA"
+    ),
+    numericInput(#how many schools users want show
+      inputId = "x_val",
+      label = "How many schools do you want to show?",
+      value = 10,
+      min = 1,
+      max = 20,
+      step = 1
+    )
+  ),
+  mainPanel(
+    tags$span(textOutput("title_3"), style = "color:red"),
+    plotlyOutput("top_school")
+  )
+)
 # Pages -------------------------------------------------------------------
 
 main_page <- tabPanel(
-    "Introduction",
-    includeCSS("css_file.css"),
+  "Introduction",
+  includeCSS("css_file.css"),
+  tags$div(
+    class = "intro",
+    h1("An Examination of Federal Student Loans"),
+    h4(class = "by", "By Luke, Ray, and Synclair"),
     tags$div(
-        class = "intro",
-        h1("An Examination of Federal Student Loans"),
-        h4(class = "by","By Luke, Ray, and Synclair"),
-        tags$div(
-            tags$p(class = "para","For our project, we are interested in looking at student loans,
+      tags$p(class = "para", "For our project, we are interested in looking at student loans,
             and asking
             several questions about them. We chose this topic because we feel
             it is
@@ -84,8 +106,8 @@ main_page <- tabPanel(
             (and student's families)
             throughout the country. It's also topical, as there has been some
             talk of student loan forgiveness under the new Biden administration."),
-            
-            tags$p(class = "para","The data we our working with is from the
+
+      tags$p(class = "para", "The data we our working with is from the
             government about the Title IV program, which concerns federal
             student aid.
             We are focusing on federal student loans, as not much data is
@@ -101,29 +123,29 @@ main_page <- tabPanel(
             government issued
             student loans. We hope to ucover some valuable insights
             through the examination of this data.")
-        )
     )
-    
+  )
 )
 
 chart_one <- tabPanel(
-    "Chart 1",
-    chart_layout
+  "Chart 1",
+  chart_layout
 )
 
 chart_two <- tabPanel(
-    "Chart 2",
-    chart2_layout,
+  "Chart 2",
+  chart2_layout,
 )
 
 chart_three <- tabPanel(
-    "Chart 3",
-    h1("Fill in later"),
+  "Chart 3",
+  h1("Find the most affordable schools"),
+  chart3_layout,
 )
 
 summary <- tabPanel(
-    "Summary",
-    h1("Fill in later"),
+  "Summary",
+  h1("Fill in later"),
 )
 
 
@@ -131,11 +153,10 @@ summary <- tabPanel(
 # Main UI -----------------------------------------------------------------
 
 shinyUI(navbarPage(
-    "Student Loans",
-    main_page,
-    chart_one,
-    chart_two,
-    chart_three,
-    summary
-    
+  "Student Loans",
+  main_page,
+  chart_one,
+  chart_two,
+  chart_three,
+  summary
 ))
